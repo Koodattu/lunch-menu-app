12b57895df9852cf7f8e416b85fbaa346eda7a76
Juha Ala-Rantala <alarantala.juha@gmail.com>
2023-04-09 15:57:59 +0300

    frontend work
    working on flutter app

--- lunch_menu_app-flutter-frontend/lib/main.dart
+++ lunch_menu_app-flutter-frontend/lib/main.dart
@@ -1,66 +1,140 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LunchMenuApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LunchMenuApp extends StatelessWidget {
+  const LunchMenuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData lightTheme = ThemeData();
+    final ThemeData darkTheme = ThemeData.dark();
+
     return MaterialApp(
       title: 'Lunch App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: lightTheme.copyWith(
+        colorScheme: lightTheme.colorScheme.copyWith(secondary: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Lunch Menu App'),
+      darkTheme: darkTheme.copyWith(
+        colorScheme: darkTheme.colorScheme.copyWith(secondary: Colors.blue),
+      ),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(title: 'Lunch Menu App'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.all(4),
+              child: Text(
+                "Today, Monday xx.xx.",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            CourseCardWidget(
+              courseName: "Salad Salad Salad Salad",
+              allergens: "G L",
+            ),
+            CourseCardWidget(
+              courseName: "Soup Soup Soup Soup",
+              allergens: "G",
+            ),
+            CourseCardWidget(
+              courseName: "Main Main Main Main",
+              allergens: "",
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class CourseCardWidget extends StatelessWidget {
+  final String courseName;
+  final String allergens;
+
+  const CourseCardWidget({super.key, required this.courseName, required this.allergens});
+
+  Icon getMenuTypeIcon(String courseName) {
+    if (courseName.toLowerCase().contains("salad")) {
+      return const Icon(
+        Icons.restaurant_menu,
+        color: Colors.green,
+        size: 40,
+      );
+    } else if (courseName.toLowerCase().contains("soup")) {
+      return const Icon(
+        Icons.restaurant,
+        color: Colors.red,
+        size: 40,
+      );
+    } else {
+      return const Icon(
+        Icons.egg,
+        color: Colors.cyan,
+        size: 40,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            getMenuTypeIcon(courseName),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(courseName),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (allergens.contains("L"))
+                      Icon(
+                        Icons.mail,
+                        color: Colors.blue.shade800,
+                      ),
+                    if (allergens.contains("G"))
+                      Icon(
+                        Icons.mail,
+                        color: Colors.orange.shade400,
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
