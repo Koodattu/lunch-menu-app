c97c2ebf66a69e8b0503e59d6d1c0581329e1fad
Juha Ala-Rantala <alarantala.juha@gmail.com>
2023-04-22 15:44:32 +0300

    frontend work
    history page refactoring

--- lunch_menu_app-flutter-frontend/lib/pages/history_page.dart
+++ lunch_menu_app-flutter-frontend/lib/pages/history_page.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:easy_localization/easy_localization.dart';
+import 'package:expandable/expandable.dart';
 import "package:http/http.dart" as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_lunch_menu_app/model/menu_week.dart';
@@ -12,7 +14,7 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> with AutomaticKeepAliveClientMixin<HistoryPage> {
-  late Future<List<MenuWeek>> menuWeek;
+  late Future<List<MenuWeek>> menuWeeks;
 
   @override
   bool get wantKeepAlive => true;
@@ -20,108 +22,140 @@ class _HistoryPageState extends State<HistoryPage> with AutomaticKeepAliveClient
   @override
   void initState() {
     super.initState();
+    getAllMenuWeeks();
+  }
 
+  getAllMenuWeeks() {
     setState(() {
-      menuWeek = fetchAll();
+      menuWeeks = fetchAll();
     });
   }
 
   Future<List<MenuWeek>> fetchAll() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:8888/api/v1/lunch-menu-weeks'));
+    final response =
+        await http.get(Uri.parse('http://10.0.2.2:8888/api/v1/lunch-menu-weeks')).timeout(const Duration(seconds: 10));
 
-    if (response.statusCode == 200) {
-      return menuWeekListFromJson(utf8.decode(response.bodyBytes));
-    } else {
-      throw Exception('Failed to load menu');
-    }
+    return menuWeekListFromJson(utf8.decode(response.bodyBytes));
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: FutureBuilder<List<MenuWeek>>(
-        future: menuWeek,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                MenuWeek menuWeek = snapshot.data![index];
-
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+    return LayoutBuilder(
+      builder: (context, constraints) => RefreshIndicator(
+        onRefresh: () async {
+          await getAllMenuWeeks();
+        },
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: FutureBuilder<List<MenuWeek>>(
+                future: menuWeeks,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
                       children: [
-                        Text(
-                          menuWeek.weekName,
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(4),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text("Salad: ${menuWeek.saladCoursePrice}"),
-                              Text("Soup: ${menuWeek.soupCoursePrice}"),
-                              Text("Main: ${menuWeek.mainCoursePrice}"),
-                            ],
-                          ),
-                        ),
                         ListView.builder(
                           shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
                           physics: const NeverScrollableScrollPhysics(),
-                          itemCount: menuWeek.menuDays.length,
                           itemBuilder: (context, index) {
-                            MenuDay menuDay = menuWeek.menuDays[index];
+                            MenuWeek menuWeek = snapshot.data![index];
 
-                            return Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(menuDay.dayName),
-                                  Padding(
-                                    padding: const EdgeInsets.all(4),
-                                    child: ListView.builder(
-                                      shrinkWrap: true,
-                                      physics: const NeverScrollableScrollPhysics(),
-                                      itemCount: menuDay.menuCourses.length,
-                                      itemBuilder: (context, index) {
-                                        MenuCourse menuCourse = menuDay.menuCourses[index];
-
-                                        return Text(menuCourse.courseName);
-                                      },
-                                    ),
+                            return Card(
+                              child: ExpandablePanel(
+                                header: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                                  child: Text(
+                                    menuWeek.weekName,
+                                    style: const TextStyle(fontSize: 20),
                                   ),
-                                ],
+                                ),
+                                collapsed: const SizedBox(),
+                                expanded: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(4),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            Text("Salad: ${menuWeek.saladCoursePrice}"),
+                                            Text("Soup: ${menuWeek.soupCoursePrice}"),
+                                            Text("Main: ${menuWeek.mainCoursePrice}"),
+                                          ],
+                                        ),
+                                      ),
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        physics: const NeverScrollableScrollPhysics(),
+                                        itemCount: menuWeek.menuDays.length,
+                                        itemBuilder: (context, index) {
+                                          MenuDay menuDay = menuWeek.menuDays[index];
+
+                                          return Padding(
+                                            padding: const EdgeInsets.all(4),
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(menuDay.dayName),
+                                                Padding(
+                                                  padding: const EdgeInsets.all(4),
+                                                  child: ListView.builder(
+                                                    shrinkWrap: true,
+                                                    physics: const NeverScrollableScrollPhysics(),
+                                                    itemCount: menuDay.menuCourses.length,
+                                                    itemBuilder: (context, index) {
+                                                      MenuCourse menuCourse = menuDay.menuCourses[index];
+
+                                                      return Text(menuCourse.courseName);
+                                                    },
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             );
                           },
                         ),
                       ],
-                    ),
-                  ),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        "error_occurred".tr(args: [snapshot.error.toString()]),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    );
+                  }
 
-          return const Center(
-            child: SizedBox(
-              width: 64,
-              height: 64,
-              child: CircularProgressIndicator(),
+                  return const Center(
+                    child: SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
